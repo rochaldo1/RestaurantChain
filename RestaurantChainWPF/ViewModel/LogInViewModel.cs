94fec340f657536444a78d7667eb2c2e8e1d294e
rochaldo1 <xusainow.artur2018@yandex.ru>
2024-12-16 22:12:52 +0700

    Unfinished LogInVM

--- RestaurantChainWPF/ViewModel/LogInViewModel.cs
+++ RestaurantChainWPF/ViewModel/LogInViewModel.cs
@@ -2,14 +2,101 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace RestaurantChainWPF.ViewModel
 {
     public class LogInViewModel : INotifyPropertyChanged
     {
+        private string _login;
+        private string _password;
+        private string _keyboardLayout;
+        private string _capsLockStatus;
+
+        private DispatcherTimer _timerForWindow = new();
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public string KeyboardLayout
+        {
+            get => _keyboardLayout;
+            set
+            {
+                _keyboardLayout = value;
+                OnPropertyChanged("KeyboardLayoutText");
+            }
+        }
+
+        public string CapsLockStatus
+        {
+            get => _capsLockStatus;
+            set
+            {
+                _capsLockStatus = value;
+                OnPropertyChanged("CapsLockStatusText");
+            }
+        }
+
+        public LogInViewModel()
+        {
+            StartTimer(100);
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
-        
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private string ParseLanguage(string language)
+        {
+            int startIndex = language.IndexOf('(');
+            int count = language.Length - startIndex;
+            language = language.Remove(startIndex, count);
+
+            return Char.ToUpper(language[0]) + language.Substring(1);
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (Console.CapsLock)
+                CapsLockStatus = "Клавиша CapsLock нажата";
+            else
+                CapsLockStatus = "";
+
+            KeyboardLayout = "Язык ввода " + ParseLanguage(InputLanguageManager.Current.CurrentInputLanguage.DisplayName);
+        }
+
+        private void StartTimer(long interval)
+        {
+            _timerForWindow.Interval = new TimeSpan(interval);
+            // TODO: добавить смену версии из assembly
+            _timerForWindow.Start();
+            _timerForWindow.Tick += TimerTick;
+        }
     }
 }
