4c931644616af4ec0ee38ebd62ec81428566ad80
rochaldo1 <xusainow.artur2018@yandex.ru>
2024-12-16 18:23:04 +0700

    Common
    password converter (to hash)

---
+++ RestaurantChain.Common/Helpers/PasswordHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantChain.Common.Helpers
+{
+    /// <summary>
+    /// Класс для конвертирования пароля в хэш код.
+    /// </summary>
+    public static class PasswordHelper
+    {
+        /// <summary>
+        /// Метод конвертирования пароля в хэш код.
+        /// </summary>
+        /// <param name="value">Пароль.</param>
+        /// <returns>Хэшированный пароль.</returns>
+        public static string GetPasswordHash(string value)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(value);
+            byte[] hashValue = MD5.HashData(messageBytes);
+            return Convert.ToHexString(hashValue);
+        }
+    }
+}
